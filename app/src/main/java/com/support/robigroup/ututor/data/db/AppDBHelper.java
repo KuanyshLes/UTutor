bc99683a556575877861ba72e7f5cf641cafa8c9
Bimurat Mukthar <bimurat.mukhtar@gmail.com>
2017-11-04 13:24:26 +0600

    mvp chat dev

---
+++ app/src/main/java/com/support/robigroup/ututor/data/db/AppDBHelper.java
@@ -0,0 +1,8 @@
+package com.support.robigroup.ututor.data.db;
+
+/**
+ * Created by Bimurat Mukhtar on 29.10.2017.
+ */
+
+public class AppDBHelper {
+}
---
+++ app/src/main/java/com/support/robigroup/ututor/data/network/ApiHelper.java
@@ -0,0 +1,18 @@
+package com.support.robigroup.ututor.data.network;
+
+public interface ApiHelper {
+
+    ApiHeader getApiHeader();
+
+//    Observable<LoginResponse> doGoogleLoginApiCall(LoginRequest.GoogleLoginRequest request);
+//
+//    Observable<LoginResponse> doFacebookLoginApiCall(LoginRequest.FacebookLoginRequest request);
+//
+//    Observable<LoginResponse> doServerLoginApiCall(LoginRequest.ServerLoginRequest request);
+//
+//    Observable<LogoutResponse> doLogoutApiCall();
+//
+//    Observable<BlogResponse> getBlogApiCall();
+//
+//    Observable<OpenSourceResponse> getOpenSourceApiCall();
+}
